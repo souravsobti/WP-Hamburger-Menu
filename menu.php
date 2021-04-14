1bf8c75d999c7fa39e5e455e493ccaa27742d067
Shorbh Sobti <souravsobti@gmail.com>
2021-04-14 15:57:38 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+
+Plugin Name: WP Hamburger Menu
+Plugin URI:  http://thenextsupportal.com/
+Description: WP Hamburger Menu
+Version:     1.0
+Author:      Shorbh Sobti
+Author URI:  http://thenextsupportal.com/
+License:     GPL2 etc
+Copyright 2021 Shorbh Sobti (email : souravsobti@gmail.com)
+*/
+
+
+
+
+
+if ( ! defined( 'NOTIFICATION_VERSION' ) ) {
+
+	define( 'NOTIFICATION_VERSION', '1.0' );
+
+}
+
+
+
+
+
+include( trailingslashit( dirname( __FILE__ ) ) . '/inc/whmenu.php' );
+
